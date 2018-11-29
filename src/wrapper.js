ee64ca4d33173374f4ba116723680664288f1ee4
byrnereese <byrne@majordojo.com>
2018-11-29 10:43:23 -0800

    partial commit for getting account ids and extensions into the URI

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,17 +1,34 @@
 var RingCentral = require('ringcentral-js-concise').default
-const RC_URLS = {
-    SMS: '/restapi/v1.0/account/~/extension/~/sms',
-    MESSAGE_LIST: '/restapi/v1.0/account/~/extension/~/message-store',
-    RING_OUT: '/restapi/v1.0/account/~/extension/~/ring-out',
-};
 
 // FIXME: need a standard method for passing in the account and extension id path parameters
+const RC_URLS = {
+    SMS:          "/restapi/v1.0/account/~/extension/~/sms",
+    MESSAGE_LIST: "/restapi/v1.0/account/~/extension/~/message-store",
+    RING_OUT:     "/restapi/v1.0/account/~/extension/~/ring-out",
+//    SMS:          `/restapi/v1.0/account/${account}/extension/${extension}/sms`,
+//    MESSAGE_LIST: `/restapi/v1.0/account/${account}/extension/${extension}/message-store`,
+//    RING_OUT:     `/restapi/v1.0/account/${account}/extension/${extension}/ring-out`,
+};
+
 // FIXME: come up with a standard way to force request input to conform to RingCentral verbose version. 
+
+function generate_rc_uri( uri, params ) {
+    // FIXME: currently doesn't do anything except remove account and extension for input params
+    // FIXME: use js templates to insert value or use default '~'
+    if ( typeof params.accountId === 'string' ) {
+	delete params.accountId
+    }
+    if ( typeof params.extension === 'string' ) {
+	delete params.extension
+    }
+    return uri
+}
 
 /*
  * This method changes in the request options structure to simplify it. 
  */
 RingCentral.prototype.sendSMS = function ( params ) {
+    let URI = generate_rc_uri( RC_URLS.SMS, params );
     // FIXME: throw an error if text exceeds 1000 characters
     // FIXME: allow "to" to be an array, FIXED: can now accept complex input using default structure
     // FIXME: default the from parameter to the current logged in user?
@@ -24,15 +41,17 @@ RingCentral.prototype.sendSMS = function ( params ) {
 	params.to = [{ phoneNumber: params.receiver }]
 	delete params.receiver
     }
-    return this.post( RC_URLS.SMS, params )
+    return this.post( URI, params )
 }
 /*
  * This method accepts the default request parameter struct.
  */
 RingCentral.prototype.getMessageList = function ( params ) {
-    return this.get( RC_URLS.MESSAGE_LIST, params )
+    let URI = generate_rc_uri( RC_URLS.MESSAGE_LIST, params );
+    return this.get( URI, params )
 }
 RingCentral.prototype.ringOut = function ( params ) {
+    let URI = generate_rc_uri( RC_URLS.RING_OUT, params );
     // FIXME: do not modify array, create a copy
     if ( typeof params.caller === 'string' ) {
 	params.from = { phoneNumber: params.caller }
@@ -49,6 +68,6 @@ RingCentral.prototype.ringOut = function ( params ) {
 	delete params.callerId
     }
     console.log( "Request params: " + JSON.stringify( params, null, 4 ) );
-    return this.post( RC_URLS.RING_OUT, params );
+    return this.post( URI, params );
 }
 module.exports.RingCentral = RingCentral
